5a34a7351c06b80cb6ec41a10bf1f511818cdf81
Michael Mescua <mescua@outlook.com>
2023-07-25 04:16:43 -0500

    feat: fetch data

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,26 @@
-import Layout from '../../components/Layout'
-import Card from '../../components/Card'
+import React from 'react';
+import Layout from '../../components/Layout';
+import Card from '../../components/Card';
 
 function Home() {
-	return <Layout>Home<Card/></Layout>;
+	const [products, setProducts] = React.useState(null);
+
+	React.useEffect(() => {
+		fetch('https://fakestoreapi.com/products')
+			.then(res => res.json())
+			.then(json => setProducts(json));
+	}, []);
+
+	return (
+		<Layout>
+			Home
+			<div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+			{products?.map(product => (
+				<Card key={product.id} {...product}  />
+			))}
+			</div>
+		</Layout>
+	);
 }
 
 export default Home;
