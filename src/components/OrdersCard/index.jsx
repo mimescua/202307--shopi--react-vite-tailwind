5b6bfc7aae6413f18c4dc0f5d1851b425c7c9350
Michael Mescua <mescua@outlook.com>
2023-07-27 01:34:07 -0500

    feat: orders list & single order

---
+++ src/components/OrdersCard/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
+
+function OrdersCard({ totalProducts, totalPrice }) {
+	return (
+		<div className="flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-80">
+			<div className='flex justify-between w-full'>
+				<p className='flex flex-col'>
+					<span className='font-light'>01.02.23</span>
+					<span className='font-light'>{totalProducts} articles</span>
+				</p>
+				<p className='flex items-center gap-2'>
+					<span className='font-medium text-2xl'>${totalPrice}</span>
+					<ChevronRightIcon
+						onClick={() => closeCheckoutSideMenu()}
+						className="h-6 w-6 text-black cursor-pointer"
+					/>
+				</p>
+			</div>
+		</div>
+	);
+}
+
+export default OrdersCard;
--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,7 +1,24 @@
-import Layout from '../../components/Layout'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ChevronLeftIcon } from '@heroicons/react/24/outline';
+import Layout from '../../components/Layout';
+import { ShoppingCartContext } from '../../context';
+import OrdersCard from '../../components/OrdersCard';
 
 function MyOrders() {
-	return <Layout>MyOrders</Layout>;
+	const { order } = React.useContext(ShoppingCartContext);
+	return (
+		<Layout>
+			<div className='flex justify-center items-center relative w-80 mb-4'>
+				<h1 className='font-medium text-xl'>My Orders</h1>
+			</div>
+			{order.map(({ totalProducts, totalPrice }, index) => (
+				<Link key={index} to={`/my-orders/${index}`}>
+					<OrdersCard totalProducts={totalProducts} totalPrice={totalPrice} />
+				</Link>
+			))}
+		</Layout>
+	);
 }
 
 export default MyOrders;
